f5ab12be939ac6ad691bee99d9342e8017ba9b79
martin1190 <martin_1190@hotmail.com>
2020-08-11 18:07:42 -0500

    Actualizacion v 1.7.6

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -124,9 +124,10 @@
                 <a href="#"><i class='fa fa-link'></i> <span>Pagos</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="{{url('Pagos')}}">Verificacion de Pagos</a></li>
-                    <li><a href="{{url('Costos')}}">Generar Reportes</a></li> 
+                    <!--<li><a href="{{url('Costos')}}">Generar Report1es</a></li> -->
+                    <li><a href="{{url('Consolidado')}}">Consolidado</a></li>
                 </ul>               
-            </li>      
+            </li>   
             <li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>Usuario</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
